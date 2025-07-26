1939015b2bba03db059c34e40c4869580e289f4e
mathiase4 <mathiaseriksson98@hotmail.com>
2025-07-26 12:21:24 +0200

    calculate

--- run.py
+++ run.py
@@ -64,37 +64,38 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
-    
-    def calculate_surplus_data(sales_row):
-        """
-        Compare sales with stock and calculate surplus data.
-        The surplus is defined as the sales figure subtracted from the stock:
-        - Positive surplus indicates waste
-        - Negastive surplus indicates extra made when stock was sold out
-    
-        """
-        print("Calculating surplus data...\n")
-        stock = SHEET.worksheet("stock").get_all_values()
-        stock_row = stock[-1]
-        print(stock_row)
+
+
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
     
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
-        surplus = stock - sales
+        surplus = int(stock) - sales
         surplus_data.append(surplus)
-        print(surplus_data)
-        
-        return surplus_data
-    
+
+    return surplus_data
+
 
 def main():
     """
     Run all program functions
     """
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
-new_surplus_data = calculate_surplus_data(sales_data)
-print(new_surplus_data)
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
+
+
 print("Welcome to Love Sandwiches Data Automation")
 main()
